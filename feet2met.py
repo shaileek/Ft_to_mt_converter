9be3e17d98cce73210fefefedb32bb08da09e078
Shailee Kotian <shailee.kotian@zeuslearning.mumbai>
2020-10-03 16:33:25 +0530

    First commit

---
+++ feet2met.py
@@ -0,0 +1,39 @@
+from tkinter import Tk,Button, Label, DoubleVar, Entry
+
+window = Tk()
+window.title("Feet to Meter Conversion")
+window.configure(background="light green")
+window.geometry("320x220")
+window.resizable(width=False,height=False)
+
+def convert():
+    value = float(ft_entry.get())
+    meter=value*0.3048
+    mt_value.set("%.4f" %meter)
+
+def clear():
+    ft_value.set("")
+    mt_value.set("")
+
+ft_lbl = Label(window, text="Feet", bg="blue", fg="white", width=14)
+ft_lbl.grid(column=0, row=0, padx=30, pady=30)
+ft_value = DoubleVar()
+ft_entry = Entry(window, textvariable=ft_value, width=14)
+ft_entry.grid(column=0, row=1)
+ft_entry.delete(0, 'end')
+
+mt_lbl = Label(window, text="Meter", bg="blue", fg="white", width=14)
+mt_lbl.grid(column=1, row=0, padx=15, pady=30)
+mt_value = DoubleVar()
+mt_entry = Entry(window, textvariable=mt_value, width=14)
+mt_entry.grid(column=1, row=1)
+mt_entry.delete(0, 'end')
+
+convert_btn = Button(window, text="Convert", bg="black", fg="white", width=14, command=convert)
+convert_btn.grid(column=0, row=3, pady=15)
+
+clear_btn = Button(window, text="Clear", bg="black", fg="white", width=14, command=clear)
+clear_btn.grid(column=1, row=3, pady=15)
+
+
+window.mainloop()
